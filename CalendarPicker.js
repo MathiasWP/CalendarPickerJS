c1f0c28432638c7666c1b22ef25afed30d8f394d
Jarmo Pertman <jarmo@jarmopertman.com>
2022-03-27 14:11:24 +0300

    Add support for specifying initial date with year less than 1970

--- CalendarPicker.js
+++ CalendarPicker.js
@@ -19,9 +19,8 @@ if (!Element.prototype.closest) {
 function CalendarPicker(element, options) {
     // Core variables.
     this.date = options.date ?
-        new Date(Math.min(
-            options.max || Infinity,
-            Math.max(options.min || 0, options.date))
+        new Date(Math.min(options.max || Infinity,
+            Math.max(options.min || new Date().setFullYear(1), options.date))
         ) : new Date();
     this._formatDateToInit(this.date);
 
